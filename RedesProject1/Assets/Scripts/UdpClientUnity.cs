e3ddeb4b88705d015f5c62a041d53839217103fd
Samyta Ramiza <145566706+SamytaS2@users.noreply.github.com>
2025-09-10 08:26:43 -0300

    aaaa

---
+++ RedesProject1/Assets/Scripts/UdpClientUnity.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+
+public class UdpClientUnity : MonoBehaviour {
+
+    UdpClient server;
+
+    IPEndPoint clientEP;
+
+    Thread receiveThread;
+
+    void Start() {
+        server = new UdpClient(5000);
+        clientEP = new IPEndPoint(IPAddress.Any, 0);
+        receiveThread = new Thread(ReceiveData);
+        receiveThread.Start();
+        Debug.Log("Servidor iniciado na porta 5000");
+
+        }
+}
